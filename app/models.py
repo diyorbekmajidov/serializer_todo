06d9287737ff68f1b03d9792559b4355968c53ba
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-11-26 10:41:19 +0500

    create User and Task

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length=255)
+    password=models.CharField(max_length=12)
+
+    def __str__(self) -> str:
+        return f'{self.username} {self.password}'
+
+class Task(models.Model):
+    name=models.CharField(max_length=64)
+    description=models.TextField()
+    status=models.BooleanField(default=False)
+    createdAt=models.DateTimeField(auto_now_add=True)
+    updateAt=models.DateTimeField(auto_now=True)
+
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='task')
+
+    def __str__(self) -> str:
+        return self.name
